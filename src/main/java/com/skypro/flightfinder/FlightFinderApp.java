3b8f2fbeab22da678ac0f3ed130bc4c623135827
garry <Garry@>
2023-04-08 07:05:46 +0300

    init

---
+++ src/main/java/com/skypro/flightfinder/FlightFinderApp.java
@@ -0,0 +1,13 @@
+package com.skypro.flightfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFinderApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightFinderApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/flightfinder/FlightFinderAppTests.java
@@ -0,0 +1,13 @@
+package com.skypro.flightfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFinderAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
